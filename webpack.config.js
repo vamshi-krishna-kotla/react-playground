2b05dc3b471a9efa987f5d2c0f7218b492df144e
vamshi-krishna-kotla <vkotla@webmd.net>
2021-06-01 20:12:17 +0530

    Added more webpack config and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,54 @@
 const path = require('path');
 
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[hash].js'
+		filename: '[name].[contenthash].js',
+		/**
+		 * cleans the content of the output folder before new build
+		 * without using webpack clean plugin
+		 */
+		clean: true
 	},
+	/**
+	 * ref:
+	 * https://stackoverflow.com/questions/54039337/how-to-remove-arrow-functions-from-webpack-output
+	 * 
+	 * about target : https://webpack.js.org/blog/2020-10-10-webpack-5-release/#improved-target
+	 */
+	target: "es5", // specifies the target JS build grammar
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				test: /\.(js|jsx)$/,
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: 'babel-loader'
+						/**
+						 * takes the presets and plugins options from
+						 * babel.config.js
+						 */
+						loader: 'babel-loader',
 					}
 				]
 			}
 		]
-	}
+	},
+	plugins: [
+		new HTMLWebpackPlugin({
+			minify: false,
+			template: './index.html',
+			/**
+			 * appends hash for query param to the script and style files
+			 * in the final HTML
+			 * useful for cache busting
+			 * 
+			 * ref: https://github.com/jantimon/html-webpack-plugin#options
+			 */
+			// hash: true
+		})
+	]
 };
