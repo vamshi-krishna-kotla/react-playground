7f44511cdcba353784b9c96ca4a04e7a9004fdc1
vamshi-krishna-kotla <vkotla@webmd.net>
2021-06-04 19:38:48 +0530

    Updated component

--- src/modules/edit-parent-state-with-functions/components/Parent/Parent.jsx
+++ src/modules/edit-parent-state-with-functions/components/Parent/Parent.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Child1 } from '../Child1/Child1.jsx';
 import { Child2 } from '../Child2/Child2.jsx';
 
+import './Parent.scss';
+
 export class Parent extends React.Component {
 	state = {};
 
 	constructor(props) {
 		super(props);
-		this.state.text = 'Parent Component';
+		this.state.text = 'children editable text';
 	}
 
 	modifyState = (value) => {
@@ -19,10 +21,17 @@ export class Parent extends React.Component {
 
 	render() {
 		return(
-			<div className="parent">
-				<h1>{this.state.text}</h1>
-				<Child1 modifyParentState={this.modifyState} />
-				<Child2 editParentState={this.modifyState}/>
+			<div id="parent">
+				<h2>This is the parent component where the text is saved</h2>
+				<h3>Enter text in the input boxes below to change the parent state</h3>
+				<h3>The following are child components</h3>
+				<h1 className="stateTextArea">
+					{this.state.text}
+				</h1>
+				<div className="childrenComponents">
+					<Child1 className="childComp" modifyParentState={this.modifyState} />
+					<Child2 className="childComp" editParentState={this.modifyState}/>
+				</div>
 			</div>
 		);
 	}
