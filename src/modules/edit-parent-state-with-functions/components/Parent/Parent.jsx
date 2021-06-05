837e0b06b2b060c61e095b1a0d6b1e5ae4f2248f
vamshi-krishna-kotla <vkotla@webmd.net>
2021-06-05 23:21:18 +0530

    Used notification component and style updates

--- src/modules/edit-parent-state-with-functions/components/Parent/Parent.jsx
+++ src/modules/edit-parent-state-with-functions/components/Parent/Parent.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import { Child1 } from '../Child1/Child1.jsx';
-import { Child2 } from '../Child2/Child2.jsx';
-
 import ReactImage from '../../../../images/logo192.png';
 
 import './Parent.scss';
 
+import { Child1 } from '../Child1/Child1.jsx';
+import { Child2 } from '../Child2/Child2.jsx';
+import { NotificationComponent } from '../Notification/Notification.jsx';
+
 export class Parent extends React.Component {
-	state = {};
+	state = {
+		notify: false
+	};
 
 	constructor(props) {
 		super(props);
@@ -16,20 +19,36 @@ export class Parent extends React.Component {
 	}
 
 	modifyState = (value) => {
+		/**
+		 * we need to remove the notification component and remount it
+		 * 
+		 * using callback inside of setState to execute
+		 * next setState synchronously
+		 */
 		this.setState({
-			text: value
+			text: value,
+			notify: false
+		}, () => {
+			this.setState({
+				notify: true
+			})
 		});
 	}
 
 	render() {
 		return(
 			<div id="parent">
-				<div className="reactImageArea">
-					<img className="reactImage" src={ReactImage} alt="React Logo" />
+				{this.state.notify ? <NotificationComponent statement="Parent state updated"/> : null}
+				<div className="parentInfo">
+					<div className="imageArea">
+						<img className="reactImage" src={ReactImage} alt="React Logo" />
+					</div>
+					<div className="infoTextArea">
+						<h2>This is the parent component where the text is saved</h2>
+						<h3>Enter text in the input boxes below to change the parent state</h3>
+						<h3>The following are child components</h3>
+					</div>
 				</div>
-				<h2>This is the parent component where the text is saved</h2>
-				<h3>Enter text in the input boxes below to change the parent state</h3>
-				<h3>The following are child components</h3>
 				<h1 className="stateTextArea">
 					{this.state.text}
 				</h1>
