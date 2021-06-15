179f6c1e610673abd4cbcea1c4a2331ab7c4c9b5
vamshi-krishna-kotla <vkotla@webmd.net>
2021-06-15 21:32:38 +0530

    Added support for SSR

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate } from 'react-dom';
 
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * render() method of ReactDOM puts the Component
+ * to the target element on the DOM
+ * 
+ * 
+ * hydrate() method of ReactDOM checks if the Component template HTML
+ * is already present (example as in SSR), then sets up the required
+ * listeners based on the Component and children scripts
+ * 
+ * hydrate() behaves same as render() if there is no SSR, i.e.,
+ * if there is no template HTML of the Component already present
+ */
+hydrate(<App />, document.getElementById('root'));
