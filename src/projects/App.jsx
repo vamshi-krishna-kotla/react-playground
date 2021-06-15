47a2e7a2e7f6217a0aa4ba6accd0b9c83004f948
vamshi-krishna-kotla <vkotla@webmd.net>
2021-06-15 20:53:36 +0530

    Added new folder for practice projects

---
+++ src/projects/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Projects</h1>
+			</div>
+		);
+	};
+}
