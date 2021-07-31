41a32854c3646b20c84eaafbff166d84eee7a224
vamshi-krishna-kotla <vkotla@webmd.net>
2021-07-31 15:45:53 +0530

    Using local variables to work with canvas

--- src/projects/analog_clock/Clock.jsx
+++ src/projects/analog_clock/Clock.jsx
@@ -1,20 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 // import the styles (as object) from the SCSS (CSS module) file
 import styles from './Clock.module.scss';
 
 export default function AnalogClock() {
 	/**
-	 * use local state to store the canvas element
+	 * create and store the canvas element
 	 */
-	let [canvas, setCanvas] = useState(null);
+	let canvas = document.createElement('canvas');
+	canvas.setAttribute('id', styles.canvas);
+	canvas.setAttribute('width', 600);
+	canvas.setAttribute('height', 600);
 
 	/**
-	 * using useEffect hook to store the canvas element
-	 * in local state
+	 * get and store the canvas context and radius
+	 */
+	let canvasContext = canvas.getContext('2d');
+	let radius = canvas.height/2;
+
+	/**
+	 * init method to start the operations on the canvas
+	 */
+	const init = () => {
+		/**
+		 * all operations on the canvas go here
+		 */
+	};
+
+	/**
+	 * using useEffect hook to start the canvas operations
+	 * after the component is mounted
 	 */
 	useEffect(() => {
-		setCanvas(document.querySelector(`#${styles.canvas}`));
+		// append the canvas to the DOM
+		document.querySelector('#analog-clock').append(canvas);
+
+		// init the canvas operations
+		init();
 	},
 	/**
 	 * pass an empty array to execute the useEffect hook
@@ -27,7 +49,6 @@ export default function AnalogClock() {
 
 	return (
 		<div id="analog-clock">
-			<canvas id={styles.canvas} width="600" height="600"></canvas>
 		</div>
 	);
 }
