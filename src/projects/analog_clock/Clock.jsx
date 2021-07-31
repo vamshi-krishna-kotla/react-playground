9eded104fecdcc8e4cf87840f9d29858c89bb3e0
vamshi-krishna-kotla <vkotla@webmd.net>
2021-07-31 16:55:59 +0530

    Added functions to draw the face and numbers of the clock

--- src/projects/analog_clock/Clock.jsx
+++ src/projects/analog_clock/Clock.jsx
@@ -16,7 +16,96 @@ export default function AnalogClock() {
 	 * get and store the canvas context and radius
 	 */
 	let canvasContext = canvas.getContext('2d');
-	let radius = canvas.height/2;
+	let radius = canvas.height / 2;
+
+	/**
+	 * fucntion to draw the face of the clock
+	 * 
+	 */
+	const drawClockFace = () => {
+		// draw the clock base
+		canvasContext.beginPath();
+		canvasContext.arc(0, 0, radius, 0, 2 * Math.PI);
+		canvasContext.fillStyle = 'white';
+		canvasContext.fill();
+
+		// prepare a radial gradient for clock border
+		let grad = canvasContext.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+		grad.addColorStop(0, '#333');
+		grad.addColorStop(0.5, 'gray');
+		grad.addColorStop(1, '#333');
+
+		// draw the clock border with radial gradient
+		canvasContext.strokeStyle = grad;
+		canvasContext.lineWidth = radius * 0.1;
+		canvasContext.stroke();
+
+		// draw and fill the center
+		canvasContext.beginPath();
+		canvasContext.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+		canvasContext.fillStyle = 'gray';
+		canvasContext.fill();
+
+		// draw border for the center
+		canvasContext.beginPath();
+		canvasContext.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+		canvasContext.strokeStyle = 'black';
+		canvasContext.lineWidth = 1;
+		canvasContext.stroke();
+	};
+
+	/**
+	 * function to draw the numbers on the clock
+	 */
+	const drawClockNumbers = () => {
+		// variables for angle and counter
+		let angle;
+		let num;
+
+		/**
+		 * set the style config to draw the numbers
+		 */
+		// font style for the numbers
+		canvasContext.font = `${radius * 0.15}px arial`;
+
+		// vertical alignment while drawing
+		canvasContext.textBaseline = 'middle';
+
+		// horizontal alignment while drawing
+		canvasContext.textAlign = 'center';
+
+		// set the font color
+		canvasContext.fillStyle = 'black';
+
+		// loop to draw the numbers onto the clock
+		for (num = 1; num <= 12; num++) {
+			// set the angle based on the number
+			angle = num * Math.PI / 6;
+
+			/**
+			 * set the position to draw the number
+			 * 
+			 * basically rotate the context and adjust the point
+			 * where the number needs to be drawn
+			 */
+			canvasContext.rotate(angle);
+			canvasContext.translate(0, -radius * 0.85);
+			canvasContext.rotate(-angle);
+
+			// draw the number
+			canvasContext.fillText(`${num}`, 0, 0);
+
+			/**
+			 * reset the position
+			 * 
+			 * this needs to be done for the next number to get the
+			 * right position
+			 */
+			canvasContext.rotate(angle);
+			canvasContext.translate(0, radius * 0.85);
+			canvasContext.rotate(-angle);
+		}
+	};
 
 	/**
 	 * init method to start the operations on the canvas
@@ -25,6 +114,23 @@ export default function AnalogClock() {
 		/**
 		 * all operations on the canvas go here
 		 */
+		canvasContext.translate(radius, radius);
+
+		/**
+		 * reducing the radius to fit the clock inside the
+		 * box without touching the edges
+		 */
+		radius *= 0.9;
+
+		/**
+		 * draw the clock face
+		 */
+		drawClockFace();
+
+		/**
+		 * draw the clock numbers
+		 */
+		drawClockNumbers();
 	};
 
 	/**
@@ -38,14 +144,14 @@ export default function AnalogClock() {
 		// init the canvas operations
 		init();
 	},
-	/**
-	 * pass an empty array to execute the useEffect hook
-	 * only when component is mouted or unmounted
-	 * 
-	 * ref: https://reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
-	 * (in the 'Note' section)
-	 */
-	[]);
+		/**
+		 * pass an empty array to execute the useEffect hook
+		 * only when component is mouted or unmounted
+		 * 
+		 * ref: https://reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
+		 * (in the 'Note' section)
+		 */
+		[]);
 
 	return (
 		<div id="analog-clock">
