7b7c7e292afe4bf735bc963e7cbc413890cbad19
VamshiKrishnaKotla <kotlavamshi999@gmail.com>
2024-08-02 14:23:45 +0530

    Updated nav bar component

--- src/projects/navbar/NavBar.jsx
+++ src/projects/navbar/NavBar.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Link } from 'react-router-dom';
 
 import './NavBar.scss';
 
 export default function () {
+	const navBarElement = useRef();
+
+	const toggleNavBar = () => {
+		navBarElement.current.classList.toggle('hide');
+		navBarElement.current.classList.toggle('show');
+	};
+
 	return (
-		<div className="navBar">
-			<Link to="/projects">Home</Link>
-			<Link to="/projects/quotes">Quotes</Link>
-			<Link to="/projects/clock">Clock</Link>
-			<Link to="/projects/speech-text">Speech &#8652; Text</Link>
-			<Link to="/projects/shop">Shop</Link>
+		<div className="nav-bar">
+			<div className="nav-bar-toggle-button button" onClick={toggleNavBar}>
+				<span>&#9776;</span>
+			</div>
+			<div className="nav-bar-options show" ref={navBarElement}>
+				<Link className="button" to="/projects">Home</Link>
+				<Link className="button" to="/projects/quotes">Quotes</Link>
+				<Link className="button" to="/projects/clock">Clock</Link>
+				<Link className="button" to="/projects/speech-text">Speech &#8652; Text</Link>
+				<Link className="button" to="/projects/shop">Shop</Link>
+			</div>
 		</div>
 	);
 };
