13994705a672a42d8f57638ba46f5596f2278bb9
VamshiKrishnaKotla <kotlavamshi999@gmail.com>
2024-08-09 14:17:19 +0530

    Finished functionality of speech recognition and conversion to text

--- src/projects/speech-text/components/speech-to-text/SpeechToText.jsx
+++ src/projects/speech-text/components/speech-to-text/SpeechToText.jsx
@@ -3,42 +3,155 @@
  * as output
  */
 
-import React, { Component } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import styles from './SpeechToText.module.scss';
+import { NotificationComponent } from "../../../../common/components/notification/Notification.jsx";
 
-export default class SpeechToText extends Component {
-	constructor(props) {
-		super(props);
+export default function SpeechToText() {
+	/**
+	 * used memoised storages for font families and speech recognition
+	 * objects to avoid multiple initialisations for evey re-render
+	 */
+	const { availableFontFamilies, speechRecognition } = useMemo(() => ({
+		availableFontFamilies: [ 'serif', 'cursive', 'aerial', 'sans'],
+		speechRecognition: new (window.SpeechRecognition || window.webkitSpeechRecognition)()
+	}), []);
 
-		this.state = {};
-
-		// set the (static) default values required for the component
-		this.defaults = {
-			continuous: false,
-			outputTranscript: 'See output here',
-		};
-
-		this.availableFontFamilies = ['Aerial', 'sans'];
-	}
+	// set the (static) default values required for the component
+	const [state, setState] = useState({
+		continuous: false,
+		output: 'See output here',
+		outputFont: availableFontFamilies[0],
+		triggerRecognition: false,
+		isUserSpeaking: false,
+		notify: false,
+		notificationStatement: ''
+	});
 
 	/**
-	 * method to make the font option available for selection
+	 * attach the event listeners for the speech recognition instance
+	 * once the component is mounted
 	 * 
-	 * @returns array of <option> elements to populate the font-selection
+	 * @note to be triggered only once
 	 */
-	getFontFamilyOptions() {
-		return this.availableFontFamilies.map(font => {
-			return <option
-						key={font}
-					>
-						{font}
-					</option>
+	useEffect(() => {
+		if (speechRecognition) {
+			speechRecognition.continuous = false;
+			speechRecognition.lang = "en-US";
+			speechRecognition.interimResults = false;
+			speechRecognition.maxAlternatives = 1;
+
+			/**
+			 * handler to retrieve conversion result
+			 * 
+			 * @param {Event} event object that holds speech conversion result
+			 */
+			speechRecognition.onresult = (event) => {
+				setState((state) => {
+					return {
+						...state,
+						output: event.results[0][0].transcript
+					};
+				});
+			};
+
+			/**
+			 * handler to update the UI to indicate speech input is being retrieved
+			 */
+			speechRecognition.onspeechstart = () => {
+				setState((state) => ({
+					...state,
+					isUserSpeaking: true
+				}));
+			};
+
+			/**
+			 * handler to stop the speech recognition listening function
+			 * when the user stops speaking
+			 */
+			speechRecognition.onspeechend = () => {
+				setState((state) => {
+					speechRecognition.stop();
+
+					return {
+						...state,
+						triggerRecognition: false,
+						isUserSpeaking: false
+					};
+				});
+			};
+
+			/**
+			 * handler to cover error case scenario when no input is received
+			 */
+			speechRecognition.onerror = () => {
+				setState((state) => {
+					return {
+						...state,
+						notify: true,
+						notificationStatement: 'Unable to detect any speech input',
+						triggerRecognition: false,
+						isUserSpeaking: false
+					};
+				});
+			};
+		} else {
+			/**
+			 * @todo this alert can be made better to notify the user
+			 * that speech recognition is not available
+			 */
+			alert('Speech recognition is not working');
+		}
+	}, []);
+
+	/**
+	 * set the output display font family from selected option
+	 * 
+	 * @param {Event} event object that holds the result of selection
+	 */
+	function setOutputFontFamily(event) {
+		event.stopPropagation();
+
+		setState((state) => {
+			return { ...state, outputFont: event.target.value };
 		});
 	}
 
-	render() {
-		return (
+	/**
+	 * set the flag to enable/disable continuous output generation
+	 * 
+	 * @param {Event} event object that holds the result of check-box
+	 */
+	function toggleContinuousOutput(event) {
+		event.stopPropagation();
+
+		setState((state) => {
+			return { ...state, continuous: !state.continuous };
+		});
+	}
+
+	/**
+	 * start listening to retrieve speech input from the user
+	 * 
+	 * @param {Event} event object to stop event propagation
+	 */
+	function startListening(event) {
+		event.stopPropagation();
+
+		setState((state) => {
+			speechRecognition.start();
+
+			return {
+				...state,
+				triggerRecognition: true
+			};
+		});
+	}
+
+	return (
+		<>
+			{state.notify ? <NotificationComponent type='error' statement={state.notificationStatement}/> : null}
 			<div className={styles["speech-to-text"]}>
 				<h1 className={styles["title"]}>Convert Speech to Text</h1>
 				<div className={styles["main-container"]}>
@@ -49,16 +162,30 @@ export default class SpeechToText extends Component {
 								type="checkbox"
 								name="continuos-output"
 								id={styles["continuos-output"]}
-								defaultChecked={this.defaults.continuous}
+								defaultChecked={state.continuous}
+								onChange={toggleContinuousOutput}
 							/>
 						</div>
 						<div className={styles["output-font-family"]}>
-							<label htmlFor={styles["font-selection"]}>Output font style : </label>
+							<label htmlFor={styles["font-selection"]}>Output font style:</label>
 							<div className={styles["inner-selection-container"]}>
-								<select name="fonts" id={styles["font-selection"]}>
-									{this.getFontFamilyOptions()}
+								<select
+									name="fonts"
+									id={styles["font-selection"]}
+									onChange={setOutputFontFamily}
+									defaultValue={availableFontFamilies[0]}
+								>
+									{
+										availableFontFamilies.map((font, index) => {
+											return <option
+														key={font + '_' + index}
+														value={font}
+													>
+														{font.toLocaleUpperCase()}
+													</option>
+										})
+									}
 								</select>
-								<section className={styles["underline-highlighter"]}></section>
 							</div>
 						</div>
 					</div>
@@ -69,20 +196,24 @@ export default class SpeechToText extends Component {
 							cols="45"
 							rows="15"
 							disabled
-							placeholder={this.defaults.outputTranscript}
+							placeholder={state.output}
+							style={{fontFamily: state.outputFont}}
 						></textarea>
-						<p className={styles["output-confidence"]}>
-							Confidence:
-						</p>
 					</div>
 				</div>
 				<div className={styles["button-container"]}>
 					<button
-						className={styles["click-to-speak"] + " button"}
-						disabled={this.state.isUserSpeaking}
+						className={
+							styles["click-to-speak"] +
+							" button " +
+							(state.triggerRecognition ? styles["restrict-speaking"] : "") + " " +
+							(state.isUserSpeaking ? styles["user-speaking"] : "")
+						}
+						disabled={state.triggerRecognition}
+						onClick={startListening}
 					>&#x1F3A4;</button>
 				</div>
 			</div>
-		);
-	}
+		</>
+	);
 }
